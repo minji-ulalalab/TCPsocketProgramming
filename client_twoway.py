9f3f1ccd5ccd733a4e281f06dd75d353a9a99365
minji-ulalalab <kmj@ulalalab.com>
2022-03-02 14:24:12 +0900

    second commit

--- client_twoway.py
+++ client_twoway.py
@@ -1,23 +1,40 @@
 #소켓 통신을 이용한 채팅프로그램 구현(클라이언트측)
-
+#first commit -> 동시대화가 불가능, 서로 대화가 불가능함
+#second commit -> 순서상관없이 동시적으로 동작하기 위해 스레드 사용
 
 from socket import *
+import threading
+import time
 
-port = 8080
+#보내는 기능과 받는 기능을 함수로 구분
+def send(sock):
+    #프로세스가 동작하는 한 스레드가 꺼지지 않게 하기 위해서 while사용
+     while True:
+        sendData = input('>>>')
+        sock.send(sendData.encode('utf-8'))
+
+def receive(sock):
+    while True:
+        recvData = sock.recv(1024)
+        print('상대방 : ', recvData.decode('utf-8'))
+
+
+port = 8081
 
 clientSock = socket(AF_INET, SOCK_STREAM)
 clientSock.connect(('127.0.0.1', port))
 
 print('접속 완료')
 
-#프로그램을 강제종료하지 않으면 계속 실행되게 함
-#먼저 받은다음에 상대방에게 송신
+#스레드 생성(target = 실행할함수, args= 전달할인자)
+sender = threading.Thread(target=send, args=(clientSock,))
+receiver = threading.Thread(target=receive, args=(clientSock,))
 
+#스레드 실행
+sender.start()
+receiver.start()
+
+#프로그램이 꺼지지 않도록 설정
 while True:
-    #채팅올때까지 대기, 채팅오면 출력
-    recvData = clientSock.recv(1024)
-    print('상대방 : ', recvData.decode('utf-8'))
-
-    #답변 입력
-    sendData = input('>>>')
-    clientSock.send(sendData.encode('utf-8'))
+    time.sleep(1)#무한루프를 쉬어가게 하는 용도
+    pass
--- server_twoway.py
+++ server_twoway.py
@@ -1,8 +1,27 @@
 #소켓 통신을 이용한 채팅프로그램 구현(서버측)
+#first commit -> 동시대화가 불가능, 서로 대화가 불가능함
+#second commit -> 순서상관없이 동시적으로 동작하기 위해 스레드 사용
 
 from socket import *
+import threading
+import time
 
-port = 8080
+#보내는 기능과 받는 기능을 함수로 구분
+#데이터 소켓을 인자로 받음
+def send(sock):
+    while True:
+        #프로세스가 동작하는 한 스레드가 꺼지지 않게 하기 위해서 while사용
+        sendData = input('>>>')
+        sock.send(sendData.encode('utf-8'))
+
+    
+def receive(sock):
+    while True:
+        recvData = sock.recv(1024)
+        print('상대방 : ', recvData.decode('utf-8'))
+
+
+port = 8081
 
 serverSock = socket(AF_INET, SOCK_STREAM)
 serverSock.bind(('', port))
@@ -10,15 +29,20 @@ serverSock.listen(1)
 
 print('%d번 포트로 접속 대기중...'%port)
 
+#client에서 접속할때까지 대기
 connectionSock, addr = serverSock.accept()
 
+#client에서 접속하면 접속한 주소 출력 되면서 연결
 print(str(addr), '에서 접속되었습니다.')
 
-#프로그램을 강제종료하지 않으면 계속 실행되게 함
-#보낸 후에 상대방으로부터 수신을 기다림
-while True:
-    sendData = input('>>>')
-    connectionSock.send(sendData.encode('utf-8'))
+#스레드 생성(target = 실행할함수, args= 전달할인자)
+sender = threading.Thread(target=send, args=(connectionSock,))
+receiver = threading.Thread(target=receive, args=(connectionSock,))
 
-    recvData = connectionSock.recv(1024)
-    print('상대방:', recvData.decode('utf-8'))
+#스레드 실행
+sender.start()
+receiver.start()
+
+while True:
+    time.sleep(1)
+    pass
